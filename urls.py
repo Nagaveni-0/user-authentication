0b0281897761e0b70c4df898872cd8a6f5ef7240
Nagaveni-0 <nagavenipujar26@gmail.com>
2026-07-27 13:35:38 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,9 +1,8 @@
-# user/urls.py
-from django.urls import path
-from .views import RegisterView, LoginView, ProfileView
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('profile/', ProfileView.as_view(), name='profile'),
+    path('admin/', admin.site.urls),
+    path('api/', include('user.urls')),
+    path('api/auth/',include('user.urls')),
 ]
